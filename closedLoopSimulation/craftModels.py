4ddd0548e43033237e8c2c4185b69ecaef5e58a6
os1315 <olaf.sikorski@gmail.com>
2020-09-23 08:55:14 +0200

    CLeaned up craft.

--- closedLoopSimulation/craftModels.py
+++ closedLoopSimulation/craftModels.py
@@ -1,65 +1,5 @@
 # A model of a craft lander
-from typing import Union
 import numpy as np
-
-
-class Lander:
-    U_MAX = 1000  # Max thrust
-    g_EARTH = 9.806  # Gravitational acceleration [ms^-2]
-    g_LUNAR = 1.623  # Gravitational acceleration [ms^-2]
-    Isp = 1000000  # Engine specific impulse
-
-    def __init__(self,
-                 init_position: Union[np.array, list] = np.array([0, 0, 0], dtype='float'),
-                 init_orientation: Union[np.array, list] = np.array([0, -90, 0], dtype='float'),
-                 init_velocity: Union[np.array, list] = np.array([0, 0, 0], dtype='float'),
-                 init_mass: float = 1000):
-
-        if type(init_position) is list:
-            self.position = np.array(init_position, dtype='float')
-        elif type(init_position) is np.array:
-            self.position = init_position.astype('float')
-        else:
-            self.position = init_position.astype(float)
-
-        if type(init_orientation) is list:
-            self.orientation = np.array(init_orientation, dtype='float')
-        elif type(init_orientation) is np.array:
-            self.orientation = init_orientation.astype('float')
-        else:
-            self.orientation = init_orientation.astype(float)
-
-        if type(init_velocity) is list:
-            self.velocity = np.array(init_velocity, dtype='float')
-        elif type(init_velocity) is np.array:
-            self.velocity = init_velocity.astype('float')
-        else:
-            self.velocity = init_velocity.astype(float)
-
-        self.mass = float(init_mass)
-
-    def update(self, u: float, dt: float, solver: bool = False) -> np.array:
-
-        # TODO: Generalise to 3d
-
-        if solver:
-            raise NotImplementedError
-        else:
-            self.position = self.position + dt * self.velocity
-            self.velocity[2] = self.velocity[2] + dt * (u / self.mass - Lander.g)
-            self.mass = self.mass - dt * u / Lander.Isp  # TODO: What is g0 in the paper?
-
-            return self.position
-
-    def verticalTime2Contact(self):
-        """Returns the real time to contact at current state of the craft"""
-
-        if self.velocity[2] != 0:
-            return self.position[2] / self.velocity
-
-        else:
-            return 1000_000
-
 
 class VerticalLander:
     """
@@ -92,6 +32,14 @@ class VerticalLander:
         self.mass = float(init_mass)
 
     def update(self, u: float, dt: float, solver: bool = False) -> np.array:
+        """
+        Updates craft position after time dt.
+
+        :param u:
+        :param dt:
+        :param solver:
+        :return:
+        """
 
         if solver:
             raise NotImplementedError
@@ -105,16 +53,18 @@ class VerticalLander:
 
             return self.position
 
-    def verticalTime2Contact(self):
+    def verticalTime2Contact(self) -> float:
         """Returns the real time to contact at current state of the craft"""
 
         try:
             return abs(self.position / self.velocity)
         except ZeroDivisionError:
-            return 1_000_000
+            return float(1_000_000)
 
 
 if __name__ == "__main__":
+
+    # Tests that the craft is falling when repeatedly calling update.
 
     import matplotlib.pyplot as plt
 
@@ -122,7 +72,7 @@ if __name__ == "__main__":
 
     time = range(10000)
     time = [t * step for t in time]
-    craft = VerticalLander(init_position=3, body='Moon')
+    craft = VerticalLander(body='Moon')
     pos = []
     M = []
 
@@ -131,19 +81,22 @@ if __name__ == "__main__":
         M.append(craft.mass)
 
     z = [-0.5 * VerticalLander.g['Moon'] * t * t for t in time]
-    error = [x1 - x2 for x1, x2 in zip(z, pos)]
+    deviation = [x1 - x2 for x1, x2 in zip(z, pos)]
 
     fig = plt.figure()
 
     ax1 = fig.add_subplot(3, 1, 1)
     ax1.plot(time, pos, label='approx')
     ax1.plot(time, z, label='exact')
+    ax1.set_title("Altitude")
     ax1.legend()
 
     ax2 = fig.add_subplot(3, 1, 2)
-    ax2.plot(time, error)
+    ax2.plot(time, deviation)
+    ax1.set_title("Deviation")
 
     ax3 = fig.add_subplot(3, 1, 3)
     ax3.plot(time, M)
+    ax1.set_title("Mass")
 
     plt.show()
